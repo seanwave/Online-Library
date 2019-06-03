8feb472a82088624d00d90869c3d6ee1523e7e60
seanwave <49344211+seanwave@users.noreply.github.com>
2019-06-03 18:12:21 -0400

    Add files via upload

---
+++ My Online Library/inc/Dataz.php
@@ -0,0 +1,184 @@
+<?php
+$catalog[10] = [];
+//Books
+$catalog[101] = [
+  "title" => "Diary of a Wimpy Kid",
+  "img" => "img/media/Wimpy.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2007,
+ "category" => "Books",
+ "author" => [
+     "Jeff Kinney",
+ ],
+ "publisher" => "Amulet Books",
+ "isbn" => '9780143303831'
+];
+$catalog[102] = [
+  "title" => "Romeo and Juliet",
+  "img" => "img/media/Romeo.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 1977,
+ "category" => "Books",
+ "author" => [
+     "William Shakespeare",
+ ],
+ "publisher" => "Pocket Classics",
+ "isbn" => '9780812416558'
+];
+$catalog[103] = [
+  "title" => "Transformers: The Covenant of Primus",
+  "img" => "img/media/Covenant.jpg",
+ "genre" => "Fiction",
+ "format" => "Hardcover",
+ "year" => 2013,
+ "category" => "Books",
+ "author" => [
+     "Justina Robson",
+ ],
+ "publisher" => "47North",
+ "isbn" => '9781477805992'
+];
+$catalog[104] = [
+  "title" => "The Hunger Games",
+  "img" => "img/media/HungerGames.jpg",
+ "genre" => "Fiction",
+ "format" => "Paperback",
+ "year" => 2010,
+ "category" => "Books",
+ "author" => [
+     "Suzanne Collins",
+ ],
+ "publisher" => "Scholastic, Inc",
+ "isbn" => '9780439023528'
+];
+//Movies
+$catalog[201] = [
+ "title" => "The Transformers: The Movie",
+ "img" => "img/media/Transformers.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 1986,
+ "category" => "Movies",
+ "director" => "Nelson Shin",
+ "writers" => [
+     "Ron Friedman"
+
+ ],
+ "stars" => [
+     "Eric Idle",
+     "Judd Nelson",
+     "Leonard Nimoy",
+     "Robert Stack",
+     "Lionel Stander",
+     "Orson Welles",
+ ]
+];
+$catalog[202] = [
+ "title" => "Drive",
+ "img" => "img/media/Drive.jpg",
+ "genre" => "Action,Adventure,Fantasy",
+ "format" => "DVD",
+ "year" => 2011,
+ "category" => "Movies",
+ "director" => "Nicolas Winding Refn",
+ "writers" => [
+     "Hossein Amini"
+
+ ],
+ "stars" => [
+     "Ryan Gosling",
+     "Carey Mulligan",
+     "Bryan Cranston",
+    "Christina Hendricks",
+    "Ron Perlman",
+    "Oscar Isaac",
+    "Albert Brooks",
+ ]
+];
+$catalog[203] = [
+ "title" => "Pulp Fiction",
+ "img" => "img/media/PulpFiction.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "DVD",
+ "year" => 1994,
+ "category" => "Movies",
+ "director" => "Quentin Tarantino",
+ "writers" => [
+     "Quentin Tarantino"
+
+ ],
+ "stars" => [
+     "John Travolta"
+     "Samuel L. Jackson"
+     "Uma Thurman"
+     "Harvey Keitel"
+     "Tim Roth"
+     "Amanda Plummer"
+     "Maria de Medeiros"
+     "Ving Rhames"
+     "Eric Stoltz"
+     "Rosanna Arquette"
+     "Christopher Walken"
+     "Bruce Willis"
+ ]
+];
+$catalog[204] = [
+ "title" => "Star Wars",
+ "img" => "img/media/StarWars.jpg",
+ "genre" => "Action,Adventure",
+ "format" => "DVD",
+ "year" => 1977,
+ "category" => "Movies",
+ "director" => "George Lucas",
+ "writers" => [
+     "George Lucas"
+
+ ],
+ "stars" => [
+     "Mark Hamill"
+     "Harrison Ford"
+     "Carrie Fisher"
+     "Peter Cushing"
+     "Alec Guinness"
+ ]
+];
+/Music
+$catalog[301] = [
+ "title" => "Graduation",
+ "img" => "img/media/Graduation.jpg",
+ "genre" => "Hip-Hop/Rap",
+ "format" => "CD",
+ "year" => 2007,
+ "category" => "Music",
+ "artist" => "Kanye West"
+];
+$catalog[302] = [
+ "title" => "My Beautiful Dark Twisted Fantasy",
+ "img" => "img/media/MBDTF.jpg",
+ "genre" => "Hip-Hop/Rap",
+ "format" => "CD",
+ "year" => 2010,
+ "category" => "Music",
+ "artist" => "Kanye West"
+];
+$catalog[303] = [
+ "title" => "Random Access Memories",
+ "img" => "img/media/RAM.jpg",
+ "genre" => "Electronic/Dance",
+ "format" => "CD",
+ "year" => 2013,
+ "category" => "Music",
+ "artist" => "Daft Punk"
+];
+$catalog[304] = [
+ "title" => "Starboy",
+ "img" => "img/media/Starboy.jpg",
+ "genre" => "Hip-Hop/R&B",
+ "format" => "CD",
+ "year" => 2016,
+ "category" => "Music",
+ "artist" => "The Weeknd"
+];
+?>
---
+++ My Online Library/inc/footer.php
@@ -0,0 +1,19 @@
+<!-- end content -->
+
+  <div class="footer">
+
+    <div class="wrapper">
+
+      <ul>
+        <li><a href="https://www.instagram.com/LaGuardiaCC/">Instagram</a></li>
+        <li><a href="https://www.facebook.com/LaGuardiaCC">Facebook</a></li>
+      </ul>
+
+      <p>&copy;<?php echo date("Y"); ?> My Online Library</p>
+
+    </div>
+
+  </div>
+
+</body>
+</html>
